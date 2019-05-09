8107283b46cb43cf002362835244e5fc87f2f89f
Roger Peppe <rogpeppe@gmail.com>
2019-05-09 17:32:13 +0100

    implement Hex128

--- uuid_test.go
+++ uuid_test.go
@@ -60,6 +60,68 @@ func TestUniqueness(t *testing.T) {
 	}
 }
 
+func TestHex128(t *testing.T) {
+	var b [24]byte
+	for i := range b {
+		b[i] = byte(i + 1)
+	}
+	got, want := Hex128(b), "01020304-0506-0708-090a-0b0c0d0e0f10"
+	if got != want {
+		t.Fatalf("unexpected Hex128 result; got %q want %q", got, want)
+	}
+}
+
+var validHex128Tests = []struct {
+	u     string
+	valid bool
+}{{
+	u:     "01020304-0506-0708-090a-0b0c0d0e0f10",
+	valid: true,
+}, {
+	u:     "01020304-0506-0708-090a-0b0c0d0e0f1",
+	valid: false,
+}, {
+	u:     "0102030430506-0708-090a-0b0c0d0e0f1",
+	valid: false,
+}, {
+	u:     "01020304-050630708-090a-0b0c0d0e0f1",
+	valid: false,
+}, {
+	u:     "01020304-0506-07084090a-0b0c0d0e0f1",
+	valid: false,
+}, {
+	u:     "01020304-0506-0708-090a50b0c0d0e0f1",
+	valid: false,
+}, {
+	u:     "01020304-0506-0708-090a-0b0c0d0e0f1",
+	valid: false,
+}, {
+	u:     "01020304-0506-0708-090a-0b0c0d0e0f102",
+	valid: false,
+}, {
+	u:     "01020304-0506-0708-090a-0b0c0d0e0f1/",
+	valid: false,
+}}
+
+func TestValidHex128(t *testing.T) {
+	for _, test := range validHex128Tests {
+		t.Run(test.u, func(t *testing.T) {
+			if got := ValidHex128(test.u); got != test.valid {
+				t.Fatalf("unexpected valid for %q; got %v want %v", test.u, got, test.valid)
+			}
+		})
+	}
+}
+
+var _s string
+
+func BenchmarkHex128(b *testing.B) {
+	g := MustNewGenerator()
+	for i := 0; i < b.N; i++ {
+		_s = Hex128(g.Next())
+	}
+}
+
 func BenchmarkNext(b *testing.B) {
 	g := MustNewGenerator()
 	for i := 0; i < b.N; i++ {
